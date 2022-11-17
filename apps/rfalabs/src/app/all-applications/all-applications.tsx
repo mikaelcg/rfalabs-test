9b04bf5b7b1bb89b5c392f1cc3a0f71032b638d0
Mikael <mikael.gallucci@escriba.com.br>
2022-11-16 22:28:13 -0300

    feat: Added all applications request

--- apps/rfalabs/src/app/all-applications/all-applications.tsx
+++ apps/rfalabs/src/app/all-applications/all-applications.tsx
@@ -2,6 +2,20 @@ import styles from './all-applications.module.scss';
 import http from '@rfalabs-test/http';
 import { AxiosResponse } from 'axios';
 import { useEffect, useState } from 'react';
+import {
+  Table,
+  Thead,
+  Tbody,
+  Tr,
+  Th,
+  Td,
+  TableContainer,
+  Container,
+  Text,
+  Center,
+  Spinner,
+} from '@chakra-ui/react';
+
 /* eslint-disable-next-line */
 export interface AllApplicationsProps {}
 
@@ -11,21 +25,59 @@ export interface Applications {
 }
 
 export function AllApplications(props: AllApplicationsProps) {
+  const [loading, setLoading] = useState<boolean>(false);
   const [applications, setApplications] = useState<Applications[]>([]);
 
   useEffect(() => {
+    setLoading(true);
+
     http.get('applications').then((response: AxiosResponse) => {
       setApplications(response.data);
+      setLoading(false);
     });
   }, []);
 
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to AllApplications!</h1>
-      {applications.map((app) => {
-        return <p key={app.id}>{app.description}</p>;
-      })}
-    </div>
+    <Container
+      maxW="container.lg"
+      bg="gray.300"
+      color="#262626"
+      h="100%"
+      w="100%"
+    >
+      <Center>
+        <Text fontSize="4xl">All Applications</Text>
+      </Center>
+
+      {loading ? (
+        <Center p={6}>
+          <Spinner />
+        </Center>
+      ) : (
+        <TableContainer>
+          <Table variant="striped" colorScheme="blackAlpha">
+            <Thead>
+              <Tr>
+                <Th>ID</Th>
+                <Th>Description</Th>
+                <Th isNumeric>Actions</Th>
+              </Tr>
+            </Thead>
+            <Tbody>
+              {applications.map((app) => {
+                return (
+                  <Tr key={app.id}>
+                    <Td>{app.id}</Td>
+                    <Td>{app.description}</Td>
+                    <Td isNumeric>+</Td>
+                  </Tr>
+                );
+              })}
+            </Tbody>
+          </Table>
+        </TableContainer>
+      )}
+    </Container>
   );
 }
 
--- apps/rfalabs/src/app/application-detail/application-detail.tsx
+++ apps/rfalabs/src/app/application-detail/application-detail.tsx
@@ -1,6 +1,6 @@
 import styles from './application-detail.module.scss';
 import { useParams } from 'react-router-dom';
-
+import { Container, Flex, Text, Divider } from '@chakra-ui/react';
 /* eslint-disable-next-line */
 export interface ApplicationDetailProps {}
 
@@ -8,9 +8,51 @@ export function ApplicationDetail(props: ApplicationDetailProps) {
   const { applicationId } = useParams();
 
   return (
-    <div className={styles['container']}>
-      <h1>Welcome to ApplicationDetail {applicationId}!</h1>
-    </div>
+    <Flex h="100%" w="100%">
+      <Container
+        maxW="container.lg"
+        bg="gray.300"
+        color="#262626"
+        h="100%"
+        w="100%"
+      >
+        <Flex
+          justify={'center'}
+          align={'center'}
+          direction="column"
+          h="100%"
+          w="100%"
+        >
+          <div className={styles['container']}>
+            <h1>Welcome to ApplicationDetail {applicationId}!</h1>
+          </div>
+          <Text>Primary</Text>
+          <Text> Application Data</Text>
+          <Text>Working Area</Text>
+        </Flex>
+      </Container>
+
+      <Divider orientation="vertical" />
+
+      <Container
+        maxW="container.lg"
+        bg="gray.300"
+        color="#262626"
+        h="100%"
+        w="100%"
+      >
+        <Flex
+          justify={'center'}
+          align={'center'}
+          direction="column"
+          h="100%"
+          w="100%"
+        >
+          <Text> Supplementary Data</Text>
+          <Text>Working Area</Text>
+        </Flex>
+      </Container>
+    </Flex>
   );
 }
 
